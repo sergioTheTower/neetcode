345dd9a26294b181544415f82fbda2a7ac0578c2
sergioTheTower <200695463+sergioTheTower@users.noreply.github.com>
2026-02-10 13:52:39 -0700

    Add Golang Valid Palindrome with tests

---
+++ two_pointers/valid_palindrome/README.md
@@ -0,0 +1,22 @@
+# Valid Palindrome
+
+| Difficulty | Tags                        |
+| :--------- | :-------------------------- |
+| **Easy**   | Topics, Company Tags, Hints |
+
+## Description
+
+Given a string `s`, return `true` if it is a **palindrome**, otherwise return `false`.
+
+> A **palindrome** is a string that reads the same forward and backward. It is also case-insensitive and ignores all non-alphanumeric characters.
+
+**Note:** Alphanumeric characters consist of letters (`A-Z`, `a-z`) and numbers (`0-9`).
+
+## Examples
+
+### Example 1
+
+```text
+Input: s = "Was it a car or a cat I saw?"
+Output: true
+```
---
+++ two_pointers/valid_palindrome/valid_palindrome.go
@@ -0,0 +1,36 @@
+// Package validpalindrome provides functionality to validate if a string is a valid palindrome.
+package validpalindrome
+
+import (
+	"strings"
+	"unicode"
+)
+
+func validPalindrome(s string) bool {
+	if len(s) == 0 {
+		return true
+	}
+	s = strings.ToLower(s)
+	left, right := 0, len(s)-1
+	for left < right {
+		if !isCharOrNumber(rune(s[left])) {
+			left++
+			continue
+		}
+		if !isCharOrNumber(rune(s[right])) {
+			right--
+			continue
+		}
+		if s[left] != s[right] {
+			return false
+
+		}
+		left++
+		right--
+	}
+	return true
+}
+
+func isCharOrNumber(r rune) bool {
+	return unicode.IsLetter(r) || unicode.IsDigit(r)
+}
